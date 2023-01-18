cf3a55675d4bea451c15030c6f6a6d2110a05e41
Seetharaman96 <seetharam.ad96@gmail.com>
2023-01-18 15:52:13 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <Msg
-        name="Adhini"
+        name="Natasha"
         pic="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
       />
       <Msg
-        name="Paari"
+        name="Joey"
         pic="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
       />
       <Msg
-        name="Vaishu"
+        name="Emma"
         pic="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
       />
     </div>
